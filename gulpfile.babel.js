50d0a2e4f433bd9c3a53004cad77cd6c13e07688
Joao Olivio (Valtech) <joao.olivio@valtech.com>
2019-11-19 10:41:20 -0300

    Setup project items.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,27 @@
-import gulp, { parallel } from 'gulp';
+import gulp, { series } from 'gulp';
 import { style } from './tasks/style';
 
 import { exec } from 'child_process';
 
 function fractal () {
+    console.log('http://localhost:3000/');
     exec('fractal start --sync', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
     });
 }
 
-function watch () {
+function watchSass () {
     gulp.watch('src/**/*.scss', style);
 }
 
 function develop () {
-    parallel(fractal, watch);
+    fractal();
+    watchSass();
 }
 
 // Don't forget to expose the task!
-exports.watch = watch;
+exports.watch = watchSass;
 exports.style = style;
 exports.fractal = fractal;
 exports.develop = develop;
